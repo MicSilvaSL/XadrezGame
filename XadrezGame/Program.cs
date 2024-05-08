e0e1ec2252fd797c2b6f539569a22bf86a05f8d1
Michael Silva <51726968+MicSilvaSL@users.noreply.github.com>
2024-05-08 09:28:37 -0300

    create position class

---
+++ XadrezGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using XadrezGame.Tabuleiro;
+
+namespace XadrezGame
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Position p = new Position(3, 4);
+
+			Console.WriteLine(p);
+		}
+	}
+}
