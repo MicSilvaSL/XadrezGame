5ced47e92702e49462ae6c53ab2160c932b61b0f
Michael Silva <51726968+MicSilvaSL@users.noreply.github.com>
2024-05-08 09:42:20 -0300

    create board, pieces and colors

---
+++ XadrezGame/Tabuleiro/Board.cs
@@ -0,0 +1,19 @@
+﻿namespace XadrezGame.Tabuleiro
+{
+	public class Board
+	{
+		public int Line { get; set; }
+		public int Colunm { get; set; }
+
+		private Piece[,] _pieces;
+
+		public Board (int  line, int colunm) 
+		{
+			this.Line = line;
+			this.Colunm = colunm;
+			
+			_pieces = new Piece[line, colunm];
+		}
+
+	}
+}
---
+++ XadrezGame/Tabuleiro/Piece.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XadrezGame.Tabuleiro
+{
+	internal class Piece
+	{
+		public Position PiecePosition { get; set; }
+		public PieceColor Color { get; set; }
+		public int AmountMovement { get; protected set; }
+		public Board CurrentBoard { get; protected set; }
+
+
+		public Piece(Position pos, PieceColor color, Board board) 
+		{
+			this.PiecePosition = pos;
+			this.Color = color;
+			this.CurrentBoard = board;
+
+			AmountMovement = 0;
+		}
+
+	}
+}
